b606f86fc236646b6fa89bd54629d15267a38715
Yasmeen <yasmeenmas@gmx.de>
2023-08-02 12:56:14 +0200

    abstracter Klasse/ Methode

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,21 @@ public class App {
     
     public static void main(String[] args) {
      
+        Rectangle rectangle = new Rectangle(10.0, 10.0);
+        Circle circle = new Circle(2.0);
+        Triangle triangle = new Triangle(4.0, 5.0 );
+
+        rectangle.area();
+        circle.area();
+        triangle.area();
+
+        output("------ Flächen ------");
+        output("Fläche Rechteck: " + rectangle.area());
+        output("Fläche Kreis: " + circle.area());
+        output("Fläche Dreieck: " + triangle.area());
+
        
     }
-
-  
-
-   
-
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
---
+++ com/cc/java/Circle.java
@@ -0,0 +1,24 @@
+package com.cc.java;
+
+public class Circle extends Shape{
+
+    private double radius;
+
+    
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+
+
+    @Override
+    public double area() {
+        // return radius * radius * Math.PI;
+        return Math.PI * Math.pow(radius, 2);
+    }
+    
+
+
+    
+}
