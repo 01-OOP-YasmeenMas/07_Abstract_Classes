67404fdcca0dd8276e4fef6ca2a23835babd4fc2
Yasmeen <yasmeenmas@gmx.de>
2023-08-03 08:58:48 +0200

    2. Iteration: Polymorphie - besser!

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -22,6 +22,8 @@ public class App {
         output("Diff. r-t: " + areaDiff(rectangle, triangle));
         output("Diff. r-c: " + areaDiff(rectangle, circle));
         output("Diff. c-r: " + areaDiff(circle,rectangle));
+        output("Diff. c-t: " + areaDiff(circle, triangle));
+        output("Diff. t-t: " + areaDiff(triangle, triangle));
        
     }
 
@@ -32,19 +34,23 @@ public class App {
     //     return c.area() - r.area();
     // }
 
-    private static double areaDiff(Circle c, Rectangle r) {
-        return c.area() - r.area();
+    // private static double areaDiff(Circle c, Rectangle r) {
+    //     return c.area() - r.area();
+    // }
+
+    // private static double areaDiff(Rectangle r, Triangle t) {
+    //     return r.area() - t.area();
+    // }
+
+    // private static double areaDiff(Rectangle r, Circle c) {
+    //     return r.area() - c.area();  
+    // }
+
+
+      // 2. Iteration: Polymorphie - besser!
+    private static double areaDiff(Shape s1, Shape s2) {
+        return s1.area() - s2.area();
     }
-
-    private static double areaDiff(Rectangle r, Triangle t) {
-        return r.area() - t.area();
-    }
-
-    private static double areaDiff(Rectangle r, Circle c) {
-        return r.area() - c.area();  
-    }
-
-
 
 
 
